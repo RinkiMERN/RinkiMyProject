a3109b2f2f904f8495fc496e3b7dcb65b8a2ff78
Rinki <rinkisharma0102@gmail.com>
2022-06-29 22:49:08 +0530

    no-changes

---
+++ mod.js
@@ -0,0 +1,13 @@
+let btn=document.querySelector(".button-mod");
+let mod1=document.querySelector(".module");
+console.log(mod1);
+console.log(btn);
+
+btn.onclick=function(){
+    // alert("hello");
+    mod1.classList.toggle("slide-m");
+//   console.log(z);
+// let z= mod1.className+="slide-m";
+// console.log(z);
+// mod1.className="slide-m";
+}
